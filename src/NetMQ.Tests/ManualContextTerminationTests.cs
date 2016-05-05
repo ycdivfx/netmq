d50e8e1b0470dbab398b80cab27c5824ef180a8c
Daniel Santana <danielgsantana@gmail.com>
2016-05-05 14:34:46 +0100

    Added beacon specific test for Context Termination

--- src/NetMQ.Tests/ManualContextTerminationTests.cs
+++ src/NetMQ.Tests/ManualContextTerminationTests.cs
@@ -10,12 +10,14 @@ namespace NetMQ.Tests
         [TestFixtureSetUp]
         public void Setup()
         {
+            NetMQConfig.BlockTermination = true;
             NetMQConfig.ManualTerminationTakeOver();
         }
 
         [TestFixtureTearDown]
         public void Cleanup()
         {
+            NetMQConfig.BlockTermination = false;
             NetMQConfig.DisableManualTermination();
         }
 
@@ -39,10 +41,60 @@ namespace NetMQ.Tests
             Assert.AreEqual(false, isTerminated);
         }
 
+        [Test]
+        public void TerminateAfterSocketsUse()
+        {
+            NetMQConfig.ContextCreate();
+            using (var rep = new ResponseSocket())
+            using (var req = new RequestSocket())
+            {
+                var port = rep.BindRandomPort("tcp://127.0.0.1");
+                req.Connect("tcp://127.0.0.1:" + port);
+
+                req.SendFrame("Hi");
+
+                CollectionAssert.AreEqual(new[] { "Hi" }, rep.ReceiveMultipartStrings());
+
+                rep.SendFrame("Hi2");
+
+                CollectionAssert.AreEqual(new[] { "Hi2" }, req.ReceiveMultipartStrings());
+            }
+            NetMQConfig.ContextTerminate();
+            var isTerminated = VerifyTermination();
+            Assert.AreEqual(true, isTerminated);
+        }
+
+        /// <summary>
+        /// Test case from <see cref="BeaconTests.SimplePublishSubscribe"/>, blocks context termination.
+        /// </summary>
+        [Test]
+        public void TerminateAfterBeaconUse()
+        {
+            using (var speaker = new NetMQBeacon())
+            using (var listener = new NetMQBeacon())
+            {
+                speaker.Configure(9999);
+
+                speaker.Publish("Hello", TimeSpan.FromMilliseconds(100));
+
+                listener.Configure(9999);
+                listener.Subscribe("H");
+
+                var message = listener.Receive();
+
+                Console.WriteLine(message.PeerAddress);
+
+                Assert.AreEqual("Hello", message.String);
+            }
+            NetMQConfig.ContextTerminate();
+            var isTerminated = VerifyTermination();
+            Assert.AreEqual(true, isTerminated);
+        }
+
         [Test, Repeat(10)]
         public void CycleCreateTerminate()
         {
-            NetMQConfig.ContextCreate(true);
+            NetMQConfig.ContextCreate();
             var isTerminated = VerifyTermination();
             Assert.AreEqual(false, isTerminated);
 
@@ -80,45 +132,9 @@ namespace NetMQ.Tests
         }
 
         [Test]
-        public void TerminateAfterSocketsUse()
-        {
-            NetMQConfig.ContextCreate(true);
-            using (var rep = new ResponseSocket())
-            using (var req = new RequestSocket())
-            using (var poller = new NetMQPoller { rep })
-            {
-                var port = rep.BindRandomPort("tcp://127.0.0.1");
-
-                req.Connect("tcp://127.0.0.1:" + port);
-
-                rep.ReceiveReady += (s, e) =>
-                {
-                    bool more;
-                    Assert.AreEqual("Hello", e.Socket.ReceiveFrameString(out more));
-                    Assert.False(more);
-
-                    e.Socket.SendFrame("World");
-                };
-
-                poller.RunAsync();
-
-                req.SendFrame("Hello");
-
-                bool more2;
-                Assert.AreEqual("World", req.ReceiveFrameString(out more2));
-                Assert.IsFalse(more2);
-
-                poller.Stop();
-            }
-            NetMQConfig.ContextTerminate();
-            var isTerminated = VerifyTermination();
-            Assert.AreEqual(true, isTerminated);
-        }
-
-        [Test]
         public void TerminateContext()
         {
-            NetMQConfig.ContextCreate(true);
+            NetMQConfig.ContextCreate();
             NetMQConfig.ContextTerminate();
             var isTerminated = VerifyTermination();
             Assert.AreEqual(true, isTerminated);
